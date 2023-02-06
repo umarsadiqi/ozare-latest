e4a3c927741725ce8c47843a008681609b9f3695
Ahmad Amin <ahmadamindev@gmail.com>
2023-02-06 17:33:15 +0500

    working on to fetch live matches for a team by team id

--- lib/features/search/repo/search_repo.dart
+++ lib/features/search/repo/search_repo.dart
@@ -3,12 +3,14 @@ import 'package:http/http.dart' as http;
 import 'package:ozare/features/search/models/team.dart';
 import 'dart:convert';
 
-class SearchRepo {
-  final Map<String, String> header = {
-    'X-RapidAPI-Key': '07585b4120mshbc941a57c6ebd11p11de9bjsn089233df6ab2',
-    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-  };
+import 'package:ozare/models/models.dart';
 
+final Map<String, String> header = {
+  'X-RapidAPI-Key': '07585b4120mshbc941a57c6ebd11p11de9bjsn089233df6ab2',
+  'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+};
+
+class SearchRepo {
   Future<List<Team>> getTeams(String query) async {
     try {
       final response = await http.get(
@@ -43,6 +45,57 @@ class SearchRepo {
     } catch (error) {
       log('Error in search_repo: ${error.toString()}');
       rethrow;
+    }
+  }
+
+  Future<Event?> getLiveMatchByTeam(int teamId) async {
+    try {
+      final response = await http.get(
+          Uri.parse(
+              'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&team=$teamId'),
+          headers: header);
+      if (response.statusCode == 200) {
+        final responseMap = jsonDecode(response.body) as Map<String, dynamic>;
+
+        if (responseMap['results'] < 1) return null;
+
+        final responses = responseMap['response'] as List;
+        if (responses.isEmpty) return null;
+
+        final data = responses.first;
+
+        // extract Event Fields
+        final int id = data['fixutre']['id'];
+
+        final int elapsedTime = data['fixture']['status']['elapsed'];
+        final String team1 = data['teams']['home']['name'];
+        final String logo1 = data['teams']['home']['logo'];
+        final int id1 = data['teams']['home']['id'];
+
+        final String team2 = data['teams']['away']['name'];
+        final String logo2 = data['teams']['away']['logo'];
+        final int id2 = data['teams']['away']['id'];
+
+        final int score1 = data['goals']['home'];
+        final int score2 = data['goals']['away'];
+
+        // Event Object
+        return Event(
+          id: '$id',
+          id1: '$id1',
+          id2: '$id2',
+          score1: '$score1',
+          score2: '$score2',
+          team1: team1,
+          team2: team2,
+          time: '$elapsedTime',
+          logo1: logo1,
+          logo2: logo2,
+        );
+      }
+    } catch (error) {
+      log('Error getting match by team id: ${error.toString()}');
+      return null;
     }
   }
 }
